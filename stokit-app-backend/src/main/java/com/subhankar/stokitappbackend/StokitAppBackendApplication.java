c809336f8752736fee35973cb4be40d55becf545
subh2312 <subhankarm2312@gmail.com>
2022-01-24 08:46:03 +0530

    initial commit

---
+++ stokit-app-backend/src/main/java/com/subhankar/stokitappbackend/StokitAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.subhankar.stokitappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokitAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokitAppBackendApplication.class, args);
+	}
+
+}
---
+++ stokit-app-backend/src/test/java/com/subhankar/stokitappbackend/StokitAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhankar.stokitappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokitAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
